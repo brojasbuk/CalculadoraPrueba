12b3337ebf946d5b2307120e4cf5c75fffc86d1f
brojasbuk <132396614+brojasbuk@users.noreply.github.com>
2023-06-22 14:28:37 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let temporal = 0;
+let operacion = '';
+var input = document.getElementById('input');
+
+function add(num){
+    if (num == 0) {
+        if (input.value[input.value.length - 1] != '0' || input.value.length > 1) {
+          input.value = input.value + num;
+        }
+      } else if (num == '.') {
+        if (input.value[input.value.length - 1] != '.' && input.value != '') {
+          input.value = input.value + num;
+        }
+      } else {
+        input.value = input.value + num;
+      }
+}
+function operacionFunction(operador){
+    operacion = operador;
+    temporal = input.value;
+    input.value = '';
+}
+function igual(){
+    if (operacion != ''){
+        input.value = eval(temporal + operacion + input.value);
+    }
+}
+function inverso(){
+    input.value = input.value * -1
+}
+function porciento(){
+    input.value = input.value / 100;
+}
+function cleanInput(){
+    if(input.value != ''){
+        input.value = '';
+
+    }else { 
+        temporal = 0;
+        operacion = '';
+    }
+}
